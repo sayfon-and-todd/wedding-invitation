a32153e377faf841456b8c7382d5554aa4aa4854
sayfon-and-todd <140432149+sayfon-and-todd@users.noreply.github.com>
2023-08-05 06:19:47 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const audio = new Audio("https://www.dropbox.com/scl/fi/5r0uoszsu7gvwo1vaeycn/Kevin_MacLeod_-_Canon_in_D_Major.mp3?rlkey=96dkksnmgj31eo5hms1idu18t&dl=1");
+const audio = new Audio("https://github.com/sayfon-and-todd/wedding-invitation/raw/main/Kevin_MacLeod_-_Canon_in_D_Major.mp3");
 const buttons = document.querySelectorAll("button");
 
 buttons.forEach(button => {
